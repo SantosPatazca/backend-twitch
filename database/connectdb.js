3373efbb560d6623852c8681bec0222633894c5d
SantosPatazca <u20201c269@upc.edu.pe>
2024-01-18 23:49:50 -0500

    Proyecto de twitch backend

---
+++ database/connectdb.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+
+try {
+    await mongoose.connect(process.env.URI_MONGO)
+    console.log('Connect DB ok 👍')
+} catch (error) {
+    console.log('Error de conexion a mongodb:' + error)
+}
